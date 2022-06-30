da9817b25cbfb81794983a0cfcab570d4aaf99e0
arjun <arjun259194@gmail.com>
2022-06-30 22:22:00 +0530

    video 9

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Navbar title="Textutils" about="About" />
-      <div className="container">
+      <div className="container my-3">
         <TextForm heading="Enter the text here" />
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,25 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text,setText] = useState("Enter text here");
-
-  const handelUpClick = ()=>{
-    console.log("button was clicked, OK");
-    setText(text.toUpperCase())
-  }
-
-  const handelOnChange = (event)=>{
-    console.log("button was clicked");
-    setText(event.target.value);
-  }
+  const [text, setText] = useState("Enter text here");
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea value={text} onChange={handelOnChange} className="form-control" id="my_box" rows="8"></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea value={text} onChange={event => {
+            setText(event.target.value);
+          }} className="form-control" id="my_box" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={() => {
+          setText(
+            text.toUpperCase()
+          )
+        }}>
+          Convert to upper
+        </button>
+        <button className="btn btn-primary mx-2" onClick={() => {
+          setText(
+            text.toLowerCase()
+          )
+        }}>
+          Convert to lower
+        </button>
+        <button className="btn btn-primary mx-2" onClick={() => {
+          setText(
+            text.split('').reverse().join('')
+          )
+        }}>
+          Reverse
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handelUpClick} >Convert</button>
-    </div>
-  )
+      <div className="container my-2">
+        <h1>Your text summary here</h1>
+        <p>characters {text.length} and {text.split(" ").length} words</p>
+        <p>Reading time: {0.008 * text.split(" ").length} minutes</p>
+        <p>preview</p>
+        <h2>{text}</h2>
+      </div>
+    </>
+  );
 }
